05940309989f94ee2a069979cf29ef2a71c26eba
Davido159 <es.davido159@gmail.com>
2025-05-23 10:12:44 -0300

    Feat: Implement Sequelize migrations and prepare backend for production deploy

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,10 @@
+#!/bin/sh
 set -e
 
 echo "Container entrypoint script started. NODE_ENV: $NODE_ENV"
 
-
 echo "Running database migrations..."
+
 npx sequelize-cli db:migrate --env production
 
 echo "Database migrations finished."
